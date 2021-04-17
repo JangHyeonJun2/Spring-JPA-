d7d9260e2723b1a192b0fbd9aa87f50715a63674
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-04-17 18:01:06 +0900

    일대일 연관관계

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -15,6 +15,14 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     @Column(name = "name")
     private String username;
 
